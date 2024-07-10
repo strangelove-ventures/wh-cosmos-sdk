wormhole-foundation/cosmos-sdk

Make staking keeper know about wormhole keeper

x/staking/keeper/keeper.go
@@ -19,17 +19,18 @@ var _ types.DelegationSet = Keeper{}
 
 // keeper of the staking store
 type Keeper struct {
-	storeKey   sdk.StoreKey
-	cdc        codec.BinaryCodec
-	authKeeper types.AccountKeeper
-	bankKeeper types.BankKeeper
-	hooks      types.StakingHooks
-	paramstore paramtypes.Subspace
+	storeKey       sdk.StoreKey
+	cdc            codec.BinaryCodec
+	authKeeper     types.AccountKeeper
+	bankKeeper     types.BankKeeper
+	wormholeKeeper types.WormholeKeeper
+	hooks          types.StakingHooks
+	paramstore     paramtypes.Subspace
 }
 
 // NewKeeper creates a new staking Keeper instance
 func NewKeeper(
-	cdc codec.BinaryCodec, key sdk.StoreKey, ak types.AccountKeeper, bk types.BankKeeper,
+	cdc codec.BinaryCodec, key sdk.StoreKey, ak types.AccountKeeper, bk types.BankKeeper, wk types.WormholeKeeper,
 	ps paramtypes.Subspace,
 ) Keeper {
 	// set KeyTable if it has not already been set
@@ -47,13 +48,18 @@ func NewKeeper(
 	}
 
 	return Keeper{
-		storeKey:   key,
-		cdc:        cdc,
-		authKeeper: ak,
-		bankKeeper: bk,
-		paramstore: ps,
-		hooks:      nil,
+		storeKey:       key,
+		cdc:            cdc,
+		authKeeper:     ak,
+		bankKeeper:     bk,
+		wormholeKeeper: wk,
+		paramstore:     ps,
+		hooks:          nil,
 	}
+}
+
+func (k Keeper) IsGuardian(ctx sdk.Context, addr sdk.ValAddress) bool {
+	return k.wormholeKeeper.IsGuardian(ctx, addr)
 }
 
 // Logger returns a module-specific logger.

strangelove-ventures/wh-cosmos-sdk

x/staking/keeper/keeper.go
@@ -22,12 +22,13 @@ var _ types.DelegationSet = Keeper{}
 
 // Keeper of the x/staking store
 type Keeper struct {
-	storeKey   storetypes.StoreKey
-	cdc        codec.BinaryCodec
-	authKeeper types.AccountKeeper
-	bankKeeper types.BankKeeper
-	hooks      types.StakingHooks
-	authority  string
+	storeKey       storetypes.StoreKey
+	cdc            codec.BinaryCodec
+	authKeeper     types.AccountKeeper
+	bankKeeper     types.BankKeeper
+	wormholeKeeper types.WormholeKeeper
+	hooks          types.StakingHooks
+	authority      string
 }
 
 // NewKeeper creates a new staking Keeper instance
@@ -36,6 +37,7 @@ func NewKeeper(
 	key storetypes.StoreKey,
 	ak types.AccountKeeper,
 	bk types.BankKeeper,
+	wk types.WormholeKeeper,
 	authority string,
 ) *Keeper {
 	// ensure bonded and not bonded module accounts are set
@@ -60,6 +62,10 @@ func NewKeeper(
 		hooks:      nil,
 		authority:  authority,
 	}
+}
+
+func (k Keeper) IsGuardian(ctx sdk.Context, addr sdk.ValAddress) bool {
+	return k.wormholeKeeper.IsGuardian(ctx, addr)
 }
 
 // Logger returns a module-specific logger.